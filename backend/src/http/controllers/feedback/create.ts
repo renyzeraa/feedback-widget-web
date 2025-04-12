010a6ec40100d391d1aed3041ea0e168667b41cd
Renan Silva <renansilvaytB@gmail.com>
2025-04-11 21:18:40 -0300

    feat: envio de email

--- backend/src/http/controllers/feedback/create.ts
+++ backend/src/http/controllers/feedback/create.ts
@@ -1,16 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import nodemailer from 'nodemailer'
 import { feedbackSchema } from '@/http/schemas/feedback-schemas';
 import { PrismaFeedbacksRepository } from '@/repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from '@/services/use-cases/submit-feedback-use-case';
 import { getMessageError } from '@/services/errors/handleMessageError';
+import { NodemailerMailAdapter } from '@/services/adapter/nodemailer/nodemailer-mail-adapter';
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const { comment, type, screenshot } = feedbackSchema.parse(request.body)
 
   try {
     const repository = new PrismaFeedbacksRepository()
-    const useCase = new SubmitFeedbackUseCase(repository)
+    const mailAdapter = new NodemailerMailAdapter()
+    const useCase = new SubmitFeedbackUseCase(repository, mailAdapter)
     const { feedback } = await useCase.execute({ type, comment, screenshot })
 
     return reply.status(201).send({
@@ -25,26 +26,3 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     })
   }
 }
-
-// const transport = nodemailer.createTransport({
-//   host: "sandbox.smtp.mailtrap.io",
-//   port: 2525,
-//   auth: {
-//     user: "a4a3de98db3927",
-//     pass: "****9fb8"
-//   }
-// });
-
-// await transport.sendMail({
-//   from: "Equipe feedback widget <oi@feedbackwiget.com>",
-//   cc: "Renan <renansilvaytb@gmail.com>",
-//   subject: "Novo feedback",
-//   html: [
-//     `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-//     `<p>Tipo do feedback: ${req.body.type}</p>`,
-//     `<p>Comentário: ${req.body.comment}</p>`,
-//     req.body.screenshot ? `<img src="${req.body.screenshot}" alt="Screenshot" />` : '',
-//     `</div>`
-//   ].join('\n')
-// })
-
--- backend/src/services/use-cases/submit-feedback-use-case.ts
+++ backend/src/services/use-cases/submit-feedback-use-case.ts
@@ -1,5 +1,6 @@
 import { FeedbackRepository } from "@/repositories/feedbacks-repository";
 import { $Enums } from "@prisma/client";
+import { MailAdapter } from "../adapter/mail-adapter";
 
 interface SubmitFeedbackUseCaseRequest {
   type: $Enums.TipoFeedback
@@ -8,12 +9,23 @@ interface SubmitFeedbackUseCaseRequest {
 }
 
 export class SubmitFeedbackUseCase {
-  constructor(private feedbacksRepository: FeedbackRepository) { }
+  constructor(private feedbacksRepository: FeedbackRepository, private mailAdapter: MailAdapter) { }
   async execute({ comment, type, screenshot }: SubmitFeedbackUseCaseRequest) {
     const feedback = await this.feedbacksRepository.create({
       type,
       comment,
       screenshot
+    })
+
+    await this.mailAdapter.sendMail({
+      subject: `Novo feedback - ${type}`,
+      body: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}" alt="Screenshot" />` : null,
+        `</div>`
+      ].join('\n')
     })
 
     return {
